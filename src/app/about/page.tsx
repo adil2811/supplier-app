23a4e526430623f8c53b7c040cbc13f62308ef4c
pankajdkoli <pankajdkoli@gmail.com>
2024-06-17 11:36:10 +0530

    remove unnecessary file's

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>this is the about sec</div>;
+}
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <h1>home page</h1>
+      <h1>Home</h1>
     </>
   );
 }
